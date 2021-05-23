00e1d1baaf69966bed031cc5edc0c227f0830e0b
Teraiman <toshi.guitar.no1.9995@gmail.com>
2021-05-23 14:22:07 +0000

    10章:Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,21 @@ class BooksController < ApplicationController
   def edit
   end
   
+  def new
+  end
+  
+  def create
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'home#top'
+  get "home/about" => "home#about" 
+  devise_for :users
+  resources :books
+
 end
